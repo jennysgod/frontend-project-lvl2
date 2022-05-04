95e0926e2b3802840891f9943a0e9f5bfea73260
Misha <jennysgod@gmail.com>
2022-05-04 20:42:46 +0300

    fix getDiff

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,11 +1,11 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parserSelector from './parsers.js';
 import buildDiffTree from './buildDiffTree.js';
 import stylish from './formatters/stylish.js';
 
-export default function getDiff(path1, path2, formatter) {
+export default function getDiff(path1, path2) {
   const getExt = (filepath) => path.extname(filepath).slice(1);
   const getData = (filepath) => fs.readFileSync(path.resolve(filepath), 'utf-8');
 
@@ -16,7 +16,7 @@ export default function getDiff(path1, path2, formatter) {
 
   const parsedData1 = parserSelector(data1, ext1); // объект
   const parsedData2 = parserSelector(data2, ext2); // объект
-  const allKeys = _.union(Object.keys(parsedData1), Object.keys(parsedData2)).sort(); // массив
+  // const allKeys = _.union(Object.keys(parsedData1), Object.keys(parsedData2)).sort(); // массив
 
   const tree = buildDiffTree(parsedData1, parsedData2);
   console.log(JSON.stringify(tree));
